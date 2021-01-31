886379367c0f3c7fbfedca8fa086890cbebe986c
Adam Klein <agklein@gmail.com>
2021-01-30 20:32:13 -0700

    Cleanup blender import, transition to working with FastIK

--- Assets/Scripts/AnimationScripts/Crawler/CrawlerAnimation.cs
+++ Assets/Scripts/AnimationScripts/Crawler/CrawlerAnimation.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrawlerLegOrigin : MonoBehaviour
+public class CrawlerAnimation : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private CrawlerMovement movement;
     void Start()
     {
+        movement = GetComponent<CrawlerMovement>();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
