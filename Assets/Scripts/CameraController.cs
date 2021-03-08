5a14be45287522ea4322d72ffc77cc3b32e2f802
Luke Leonard <lrleonar@calpoly.edu>
2021-03-08 15:57:45 -0800

    1_red_1_yell level

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public bool cursorLocked = true;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        if (cursorLocked)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
