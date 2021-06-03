39c794acddd936d090dd69172b0d1b7f103626e8
Luke Leonard <lrleonar@calpoly.edu>
2021-06-03 09:36:00 -0700

    hand anmiation speed fixes

--- Assets/Scripts/Interfaces/HandInterface.cs
+++ Assets/Scripts/Interfaces/HandInterface.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class HandInterface : MonoBehaviour
 {
-    private float curlSpeed = 0.002f;
-    private float curlAmount = 0.005f;
+    private float leftCurlSpeed = 0.01f;
+    private float curlAmount;
+    private float curlCount = 20f;
     private HandCurler _leftHandCurler;
     private HandCurler _rightHandCurler;
     private bool _leftHandCurled = false;
@@ -15,104 +16,19 @@ public class HandInterface : MonoBehaviour
     {
         _leftHandCurler = GameObject.FindGameObjectWithTag("Player").transform.Find("Graphics/Hands/PlayerLeftHand").GetComponentInChildren<HandCurler>();
         _rightHandCurler = GameObject.FindGameObjectWithTag("Player").transform.Find("Graphics/Hands/PlayerRightHand").GetComponentInChildren<HandCurler>();
+        curlAmount = 0.5f / curlCount;
     }
 
+    // ==============================================================
+    //                      CURLING THE HANDS
+    // ==============================================================
     public void curlLeftHand()
     {
         if (_leftHandCurled == false)
         {
             _leftHandCurled = true;
-            StartCoroutine(curlLeftHandRoutine());
+            StartCoroutine(curlHandRoutine(_leftHandCurler));
         }
-    }
-
-    private IEnumerator curlLeftHandRoutine()
-    {
-        int count = -1;
-        while (count < 100)
-        {
-            count++;
-            _leftHandCurler.arcSet = _leftHandCurler.arcSet + curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
-        }
-    }
-    public void uncurlLeftHand()
-    {
-        if (_leftHandCurled == true)
-        {
-            _leftHandCurled = false;
-            StartCoroutine(uncurlLeftHandRoutine());
-        }
-    }
-
-    private IEnumerator uncurlLeftHandRoutine()
-    {
-        int count = -1;
-        while (count < 100)
-        {
-            count++;
-            _leftHandCurler.arcSet = _leftHandCurler.arcSet - curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
-        }
-    }
-
-    public void curlAndUncurlLeftHand()
-    {
-        if (_leftHandCurled == false)
-        {
-            _leftHandCurled = true;
-            StartCoroutine(curlAndUncurlLeftHandRoutine());
-        }
-    }
-
-    private IEnumerator curlAndUncurlLeftHandRoutine()
-    {
-        int count = -1;
-        while (count < 100)
-        {
-            count++;
-            _leftHandCurler.arcSet = _leftHandCurler.arcSet + curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
-        }
-        yield return new WaitForSeconds(0.2f);
-        count = -1;
-        while (count < 100)
-        {
-            count++;
-            _leftHandCurler.arcSet = _leftHandCurler.arcSet - curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
-        }
-        _leftHandCurled = false;
-    }
-
-    public void uncurlAndCurlLeftHand()
-    {
-        if (_leftHandCurled == true)
-        {
-            _leftHandCurled = false;
-            StartCoroutine(uncurlAndCurlLeftHandRoutine());
-        }
-    }
-
-    private IEnumerator uncurlAndCurlLeftHandRoutine()
-    {
-        int count = -1;
-        _leftHandCurler.arcSet = 0.5f;
-        while (count < 100)
-        {
-            count++;
-            _leftHandCurler.arcSet = _leftHandCurler.arcSet - curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
-        }
-        count = -1;
-        while (count < 100)
-        {
-            count++;
-            _leftHandCurler.arcSet = _leftHandCurler.arcSet + curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
-        }
-        _leftHandCurler.arcSet = 0.5f;
-        _leftHandCurled = true;
     }
 
     public void curlRightHand()
@@ -120,65 +36,180 @@ public class HandInterface : MonoBehaviour
         if (_rightHandCurled == false)
         {
             _rightHandCurled = true;
-            StartCoroutine(curlRightHandRoutine());
+            StartCoroutine(curlHandRoutine(_rightHandCurler));
         }
     }
 
-    private IEnumerator curlRightHandRoutine()
+    private IEnumerator curlHandRoutine(HandCurler hand)
     {
         int count = -1;
-        while (count < 100)
+        while (count < curlCount)
         {
             count++;
-            _rightHandCurler.arcSet = _rightHandCurler.arcSet + curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
+            hand.arcSet = hand.arcSet + curlAmount;
+            yield return new WaitForSeconds(leftCurlSpeed);
         }
     }
+
+    // ==============================================================
+    //                      UNCURLING THE HANDS
+    // ==============================================================
+
+    public void uncurlLeftHand()
+    {
+        if (_leftHandCurled == true)
+        {
+            _leftHandCurled = false;
+            StartCoroutine(uncurlHandRoutine(_leftHandCurler));
+        }
+    }
+
     public void uncurlRightHand()
     {
         if (_rightHandCurled == true)
         {
             _rightHandCurled = false;
-            StartCoroutine(uncurlRightHandRoutine());
+            StartCoroutine(uncurlHandRoutine(_rightHandCurler));
         }
     }
 
-    private IEnumerator uncurlRightHandRoutine()
+    private IEnumerator uncurlHandRoutine(HandCurler hand)
     {
         int count = -1;
-        while (count < 100)
+        while (count < curlCount)
         {
             count++;
-            _rightHandCurler.arcSet = _rightHandCurler.arcSet - curlAmount;
-            yield return new WaitForSeconds(curlSpeed);
+            hand.arcSet = hand.arcSet - curlAmount;
+            yield return new WaitForSeconds(leftCurlSpeed);
         }
     }
 
-    public void uncurlAndCurlRightHand(float speed)
+    // ==============================================================
+    //                      CURL THEN UNCURL THE HANDS
+    // ==============================================================
+
+    public void curlAndUncurlLeftHand()
+    {
+        if (_leftHandCurled == false)
+        {
+            _leftHandCurled = true;
+            StartCoroutine(curlAndUncurlHandRoutine(_leftHandCurler));
+        }
+    }
+
+    private IEnumerator curlAndUncurlHandRoutine(HandCurler hand)
+    {
+        int count = -1;
+        while (count < curlCount)
+        {
+            count++;
+            hand.arcSet = hand.arcSet + curlAmount;
+            yield return new WaitForSeconds(leftCurlSpeed);
+        }
+        yield return new WaitForSeconds(0.2f);
+        count = -1;
+        while (count < curlCount)
+        {
+            count++;
+            hand.arcSet = hand.arcSet - curlAmount;
+            yield return new WaitForSeconds(leftCurlSpeed);
+        }
+        _leftHandCurled = false;
+    }
+
+    // ==============================================================
+    //                      UNCURL THEN CURL THE HANDS
+    // ==============================================================
+
+    public void uncurlAndCurlLeftHand()
+    {
+        if (_leftHandCurled == true)
+        {
+            _leftHandCurled = false;
+            StartCoroutine(uncurlAndCurlHandRoutine(_leftHandCurler));
+        }
+    }
+
+    private IEnumerator uncurlAndCurlHandRoutine(HandCurler hand)
+    {
+        int count = -1;
+        hand.arcSet = 0.5f;
+        while (count < curlCount)
+        {
+            count++;
+            hand.arcSet = hand.arcSet - curlAmount;
+            yield return new WaitForSeconds(leftCurlSpeed);
+        }
+        count = -1;
+        while (count < curlCount)
+        {
+            count++;
+            hand.arcSet = hand.arcSet + curlAmount;
+            yield return new WaitForSeconds(leftCurlSpeed);
+        }
+        hand.arcSet = 0.5f;
+        _leftHandCurled = true;
+    }
+
+    public void uncurlAndCurlRightHandRed()
     {
         if (_rightHandCurled == true)
         {
             _rightHandCurled = false;
-            StartCoroutine(uncurlAndCurlRightHandRoutine(speed));
+            StartCoroutine(uncurlAndCurlRightHandRedRoutine());
         }
     }
 
-    private IEnumerator uncurlAndCurlRightHandRoutine(float speed)
+    private IEnumerator uncurlAndCurlRightHandRedRoutine()
     {
         int count = -1;
-        while (count < 50)
+        _rightHandCurler.arcSet = 0.5f;
+        while (count < 8)
         {
             count++;
-            _rightHandCurler.arcSet = _rightHandCurler.arcSet - 0.01f;
-            yield return new WaitForSeconds(speed);
+            _rightHandCurler.arcSet = _rightHandCurler.arcSet - 0.5f/8;
+            yield return new WaitForSeconds(0.008f);
         }
         count = -1;
-        while (count < 50)
+        _rightHandCurler.arcSet = 0f;
+        while (count < 8)
         {
             count++;
-            _rightHandCurler.arcSet = _rightHandCurler.arcSet + 0.01f;
-            yield return new WaitForSeconds(speed);
+            _rightHandCurler.arcSet = _rightHandCurler.arcSet + 0.5f/8;
+            yield return new WaitForSeconds(0.008f);
         }
+        _rightHandCurler.arcSet = 0.5f;
+        _rightHandCurled = true;
+    }
+
+    public void uncurlAndCurlRightHandBlue()
+    {
+        if (_rightHandCurled == true)
+        {
+            _rightHandCurled = false;
+            StartCoroutine(uncurlAndCurlRightHandBlueRoutine());
+        }
+    }
+
+    private IEnumerator uncurlAndCurlRightHandBlueRoutine()
+    {
+        int count = -1;
+        _rightHandCurler.arcSet = 0.5f;
+        while (count < curlCount)
+        {
+            count++;
+            _rightHandCurler.arcSet = _rightHandCurler.arcSet - curlAmount;
+            yield return new WaitForSeconds(0.01f);
+        }
+        count = -1;
+        _rightHandCurler.arcSet = 0f;
+        while (count < curlCount)
+        {
+            count++;
+            _rightHandCurler.arcSet = _rightHandCurler.arcSet + curlAmount;
+            yield return new WaitForSeconds(0.01f);
+        }
+        _rightHandCurler.arcSet = 0.5f;
         _rightHandCurled = true;
     }
 }
