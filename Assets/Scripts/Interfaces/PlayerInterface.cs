b91bae8d5f23c723891a70d0143e370d3c3effee
Luke Leonard <lrleonar@calpoly.edu>
2021-06-03 21:35:42 -0700

    finish level3 and final additions

--- Assets/Scripts/Interfaces/PlayerInterface.cs
+++ Assets/Scripts/Interfaces/PlayerInterface.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
 
 public class PlayerInterface : MonoBehaviour
@@ -8,6 +9,8 @@ public class PlayerInterface : MonoBehaviour
     private LevelManager _manager;
     private ActivateResource _tethering;
     private PlayerHealthVisuals _healthVisuals;
+    private Vignette _vignette;
+    private PostProcessVolume volume;
 
     private Vector3 _respawnPoint = new Vector3(50.85f, 211.08f, -97.22f);
     // Start is called before the first frame update
@@ -17,6 +20,16 @@ public class PlayerInterface : MonoBehaviour
         _manager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
         _tethering = GetComponent<ActivateResource>();
         _healthVisuals = GameObject.FindGameObjectWithTag("VisualManager").transform.GetComponentInChildren<PlayerHealthVisuals>();
+        volume = GameObject.FindGameObjectWithTag("VisualManager").transform.Find("DeathEffects").GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out _vignette);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            killPlayer();
+        }
     }
 
     public void takeDamage(float damage) 
@@ -26,15 +39,37 @@ public class PlayerInterface : MonoBehaviour
 
     public void killPlayer()
     {
-        transform.position = _respawnPoint;
         _tethering.deactivateAll();
         _healthVisuals.setHealth(1f);
         _playerHealth.resetHealth();
+        StartCoroutine(playerDeathAnimation());
     }
 
     public void checkpoint(Vector3 respawnPoint)
     {
         _respawnPoint = respawnPoint;
         Debug.Log("Checkpoint set");
+    }
+
+    private IEnumerator playerDeathAnimation()
+    {
+        int count = 0;
+        _vignette.intensity.value = 0f;
+        volume.priority = 2;
+        while (count < 50)
+        {
+            count++;
+            _vignette.intensity.value = _vignette.intensity.value + 0.02f;
+            yield return new WaitForSeconds(0.02f);
+        }
+        transform.position = _respawnPoint;
+        count = 0;
+        while (count < 50)
+        {
+            count++;
+            _vignette.intensity.value = _vignette.intensity.value - 0.02f;
+            yield return new WaitForSeconds(0.02f);
+        }
+        volume.priority = 0;
     }
 }
